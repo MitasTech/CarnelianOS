033e1da9a0fc0c5db8c936d4678beea3ed3fb7c5
SteliosMitas <77465022+MitasTech@users.noreply.github.com>
2022-04-18 03:45:39 +0300

    first commit

---
+++ scripts/3-post-setup.sh
@@ -0,0 +1,357 @@
+#!/usr/bin/env bash
+#github-action genshdoc
+echo -ne "
+-------------------------------------------------------------------------
+   █████╗ ██████╗  ██████╗██╗  ██╗    ███╗   ███╗██╗████████╗ █████╗ ███████╗
+  ██╔══██╗██╔══██╗██╔════╝██║  ██║    ████╗ ████║██║╚══██╔══╝██╔══██╗██╔════╝
+  ███████║██████╔╝██║     ███████║    ██╔████╔██║██║   ██║   ███████║███████╗
+  ██╔══██║██╔══██╗██║     ██╔══██║    ██║╚██╔╝██║██║   ██║   ██╔══██║╚════██║
+  ██║  ██║██║  ██║╚██████╗██║  ██║    ██║ ╚═╝ ██║██║   ██║   ██║  ██║███████║
+  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚═╝     ╚═╝╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+                        SCRIPTHOME: CarnelianOS
+-------------------------------------------------------------------------
+
+Final Setup and Configurations
+GRUB EFI Bootloader Install & Check
+"
+source ${HOME}/CarnelianOS/configs/setup.conf
+
+if [[ -d "/sys/firmware/efi" ]]; then
+    grub-install --efi-directory=/boot ${DISK}
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+               Creating (and Theming) Grub Boot Menu
+-------------------------------------------------------------------------
+"
+# set kernel parameter for decrypting the drive
+if [[ "${FS}" == "luks" ]]; then
+sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"%GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=${ENCRYPTED_PARTITION_UUID}:ROOT root=/dev/mapper/ROOT %g" /etc/default/grub
+fi
+# set kernel parameter for adding splash screen
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& splash /' /etc/default/grub
+
+echo -e "Installing Arcade Grub theme..."
+THEME_DIR="/boot/grub/themes"
+THEME_NAME=Arcade
+echo -e "Creating the theme directory..."
+mkdir -p "${THEME_DIR}/${THEME_NAME}"
+echo -e "Copying the theme..."
+cd ${HOME}/CarnelianOS
+cp -a configs${THEME_DIR}/${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
+echo -e "Backing up Grub config..."
+cp -an /etc/default/grub /etc/default/grub.bak
+echo -e "Setting the theme as the default..."
+grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
+echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
+echo -e "Updating grub..."
+grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "All set!"
+
+echo -ne "
+-------------------------------------------------------------------------
+               Enabling (and Theming) Login Display Manager
+-------------------------------------------------------------------------
+"
+if [[ ${DESKTOP_ENV} == "kde" ]]; then
+  systemctl enable lightdm.service
+  if [[ ${INSTALL_TYPE} == "FULL" ]]; then
+    #echo [Theme] >>  /etc/sddm.conf
+    #echo Current=Nordic >> /etc/sddm.conf
+    systemctl enable lightdm.service
+  fi
+
+elif [[ "${DESKTOP_ENV}" == "gnome" ]]; then
+  systemctl enable lightdm.service
+elif [[ "${DESKTOP_ENV}" == "lxde" ]]; then
+  systemctl enable lxdm.service
+
+elif [[ "${DESKTOP_ENV}" == "openbox" ]]; then
+  systemctl enable lightdm.service
+  if [[ "${INSTALL_TYPE}" == "FULL" ]]; then
+    # Set default lightdm-webkit2-greeter theme to Litarvan
+    sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = litarvan #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+    # Set default lightdm greeter to lightdm-webkit2-greeter
+    sed -i 's/#greeter-session=example.*/greeter-session=lightdm-webkit2-greeter/g' /etc/lightdm/lightdm.conf
+  fi
+
+else
+  if [[ ! "${DESKTOP_ENV}" == "server"  ]]; then
+  sudo pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
+  systemctl enable lightdm.service
+  fi
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Enabling Essential Services
+-------------------------------------------------------------------------
+"
+systemctl enable cups.service
+echo "  Cups enabled"
+ntpd -qg
+systemctl enable ntpd.service
+echo "  NTP enabled"
+systemctl disable dhcpcd.service
+echo "  DHCP disabled"
+systemctl stop dhcpcd.service
+echo "  DHCP stopped"
+systemctl enable NetworkManager.service
+echo "  NetworkManager enabled"
+systemctl enable bluetooth
+echo "  Bluetooth enabled"
+systemctl enable avahi-daemon.service
+echo "  Avahi enabled"
+
+if [[ "${FS}" == "luks" || "${FS}" == "btrfs" ]]; then
+echo -ne "
+-------------------------------------------------------------------------
+                    Creating Snapper Config
+-------------------------------------------------------------------------
+"
+
+SNAPPER_CONF="$HOME/CarnelianOS/configs/etc/snapper/configs/root"
+mkdir -p /etc/snapper/configs/
+cp -rfv ${SNAPPER_CONF} /etc/snapper/configs/
+
+SNAPPER_CONF_D="$HOME/CarnelianOS/configs/etc/conf.d/snapper"
+mkdir -p /etc/conf.d/
+cp -rfv ${SNAPPER_CONF_D} /etc/conf.d/
+
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+               Enabling (and Theming) Plymouth Boot Splash
+-------------------------------------------------------------------------
+"
+PLYMOUTH_THEMES_DIR="$HOME/CarnelianOS/configs/usr/share/plymouth/themes"
+PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
+mkdir -p /usr/share/plymouth/themes
+echo 'Installing Plymouth theme...'
+cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
+if  [[ $FS == "luks"]]; then
+  sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
+  sed -i 's/HOOKS=(base udev \(.*block\) /&plymouth-/' /etc/mkinitcpio.conf # create plymouth-encrypt after block hook
+else
+  sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
+fi
+plymouth-set-default-theme -R arch-glow # sets the theme and runs mkinitcpio
+echo 'Plymouth theme installed'
+cp -r ~/zsh/.zshrc ~/
+cp -r ~/CarnelianOS/configs/.bashrc ~/
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Cleaning
+-------------------------------------------------------------------------
+"
+# Remove no password sudo rights
+sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+# Add sudo rights
+sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+rm -r $HOME/CarnelianOS
+rm -r /home/$USERNAME/CarnelianOS
+
+
+echo -ne "
+---------------------------------------------------------------------------
+				DTOS
+---------------------------------------------------------------------------
+"
+#!/usr/bin/env bash
+#  ____ _____ ___  ____
+# |  _ \_   _/ _ \/ ___|   Derek Taylor (DistroTube)
+# | | | || || | | \___ \   http://www.youtube.com/c/DistroTube
+# | |_| || || |_| |___) |  http://www.gitlab.com/dwt1/dtos
+# |____/ |_| \___/|____/
+#
+# NAME: DTOS
+# DESC: An installation and deployment script for DT's Xmonad desktop.
+# WARNING: Run this script at your own risk.
+# DEPENDENCIES: dialog
+
+if [ "$(id -u)" = 0 ]; then
+    echo "##################################################################"
+    echo "This script MUST NOT be run as root user since it makes changes"
+    echo "to the \$HOME directory of the \$USER executing this script."
+    echo "The \$HOME directory of the root user is, of course, '/root'."
+    echo "We don't want to mess around in there. So run this script as a"
+    echo "normal user. You will be asked for a sudo password when necessary."
+    echo "##################################################################"
+    exit 1
+fi
+
+error() { \
+    clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;
+}
+
+echo "################################################################"
+echo "## Syncing the repos and installing 'dialog' if not installed ##"
+echo "################################################################"
+sudo pacman --noconfirm --needed -Syu dialog || error "Error syncing the repos."
+
+welcome() { \
+    dialog --colors --title "\Z7\ZbInstalling Carnelian OS!" --msgbox "\Z4This is a script that will install what I sarcastically call Carnelian OS! (Stelios Mitas aka MitasTech's operating system).  It's really just an installation script for those that want to try out my XMonad desktop.  We will add Carnelian OS repos to Pacman and install the XMonad tiling window manager, the Xmobar panel, the Kitty terminal, the zsh shell, Doom Emacs and many other essential programs needed to make my dotfiles work correctly.\\n\\n-(Stelios Mitas, aka MitasTech aka MiTech)" 16 60
+
+    dialog --colors --title "\Z7\ZbStay near your computer!" --yes-label "Continue" --no-label "Exit" --yesno "\Z4This script is not allowed to be run as root, but you will be asked to enter your sudo password at various points during this installation. This is to give PACMAN the necessary permissions to install the software.  So stay near the computer." 8 60
+}
+
+welcome || error "User choose to exit."
+
+speedwarning() { \
+    dialog --colors --title "\Z7\ZbInstalling Carnelian OS!" --yes-label "Continue" --no-label "Exit" --yesno  "\Z4WARNING! The ParallelDownloads option is not enabled in /etc/pacman.conf. This may result in slower installation speeds. Are you sure you want to continue?" 16 60 || error "User choose to exit."
+}
+
+distrowarning() { \
+    dialog --colors --title "\Z7\ZbInstalling Carnelian OS!" --yes-label "Continue" --no-label "Exit" --yesno  "\Z4WARNING! While this script works on all Arch based distros, some distros choose to package certain things that we also package, please look at the package list and remove conflicts manually.Are you sure you want to continue?" 16 60 || error "User choose to exit."
+}
+
+grep -qs "#ParallelDownloads" /etc/pacman.conf && speedwarning
+grep -qs "ID=arch" /etc/os-release || distrowarning 
+
+lastchance() { \
+    dialog --colors --title "\Z7\ZbInstalling Carnelian OS!" --msgbox "\Z4WARNING! The Carnelian OS installation script is currently in public beta testing. There are almost certainly errors in it; therefore, it is strongly recommended that you not install this on production machines. It is recommended that you try this out in either a virtual machine or on a test machine." 16 60
+
+    dialog --colors --title "\Z7\ZbAre You Sure You Want To Do This?" --yes-label "Begin Installation" --no-label "Exit" --yesno "\Z4Shall we begin installing Carnelian OS?" 8 60 || { clear; exit 1; }
+}
+
+lastchance || error "User choose to exit."
+
+
+addrepo() { \
+    echo "#########################################################"
+    echo "## Adding the DTOS core repository to /etc/pacman.conf ##"
+    echo "#########################################################"
+    grep -qxF "[dtos-core-repo]" /etc/pacman.conf ||
+        (echo "[dtos-core-repo]"; echo "SigLevel = Required DatabaseOptional"; \
+        echo "Server = https://gitlab.com/dwt1/\$repo/-/raw/main/\$arch") | sudo tee -a /etc/pacman.conf
+}
+
+addrepo || error "Error adding DTOS repo to /etc/pacman.conf."
+
+addkeyserver() { \
+    echo "#######################################################"
+    echo "## Adding keyservers to /etc/pacman.d/gnupg/gpg.conf ##"
+    echo "#######################################################"
+    grep -qxF "keyserver.ubuntu.com:80" /etc/pacman.d/gnupg/gpg.conf || echo "keyserver hkp://keyserver.ubuntu.com:80" | sudo tee -a /etc/pacman.d/gnupg/gpg.conf
+    grep -qxF "keyserver.ubuntu.com:443" /etc/pacman.d/gnupg/gpg.conf || echo "keyserver hkps://keyserver.ubuntu.com:443" | sudo tee -a /etc/pacman.d/gnupg/gpg.conf
+}
+
+addkeyserver || error "Error adding keyservers to /etc/pacman.d/gnupg/gpg.conf"
+
+receive_key() { \
+    local _pgpkey="C71486C31555B12E"
+    echo "#####################################"
+    echo "## Adding PGP key $_pgpkey ##"
+    echo "#####################################"
+    sudo pacman-key --recv-key $_pgpkey
+    sudo pacman-key --lsign-key $_pgpkey
+}
+
+receive_key || error "Error receiving PGP key $_pgpkey"
+
+# Let's install each package listed in the pkglist.txt file.
+sudo pacman --needed --ask 4 -Sy - < pkglist.txt || error "Failed to install required packages"
+
+echo "################################################################"
+echo "## Copying DTOS configuration files from /etc/dtos into \$HOME ##"
+echo "################################################################"
+[ ! -d /etc/dtos ] && sudo mkdir /etc/dtos
+[ -d /etc/dtos ] && mkdir ~/dtos-backup-$(date +%Y.%m.%d-%H%M) && cp -Rf /etc/dtos ~/dtos-backup-$(date +%Y.%m.%d-%H%M)
+[ ! -d ~/.config ] && mkdir ~/.config
+[ -d ~/.config ] && mkdir ~/.config-backup-$(date +%Y.%m.%d-%H%M) && cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H%M)
+cd /etc/dtos && cp -Rf . ~ && cd -
+
+# Change all scripts in .local/bin to be executable.
+find $HOME/.local/bin -type f -print0 | xargs -0 chmod 775
+
+echo "#########################################################"
+echo "## Installing Doom Emacs. This may take a few minutes. ##"
+echo "#########################################################"
+[ -d ~/.emacs.d ] && mv ~/.emacs.d ~/.emacs.d.bak.$(date +"%Y%m%d_%H%M%S")
+[ -f ~/.emacs ] && mv ~/.emacs ~/.emacs.bak.$(date +"%Y%m%d_%H%M%S")
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom -y install
+~/.emacs.d/bin/doom sync
+
+[ ! -d /etc/pacman.d/hooks ] && sudo mkdir /etc/pacman.d/hooks
+sudo cp /etc/dtos/.xmonad/pacman-hooks/recompile-xmonad.hook /etc/pacman.d/hooks/
+sudo cp /etc/dtos/.xmonad/pacman-hooks/recompile-xmonadh.hook /etc/pacman.d/hooks/
+
+[ ! -d $HOME/.config/dmscripts ] && mkdir $HOME/.config/dmscripts
+cp /etc/dmscripts/config $HOME/.config/dmscripts/config
+sed -i 's/DMBROWSER=\"brave\"/DMBROWSER=\"qutebrowser\"/g' $HOME/.config/dmscripts/config
+sed -i 's/DMTERM=\"st -e\"/DMTERM=\"alacritty -e\"/g' $HOME/.config/dmscripts/config
+sed -i 's/setbg_dir=\"${HOME}\/Pictures\/Wallpapers\"/setbg_dir=\"\/usr\/share\/backgrounds\/dtos-backgrounds\"/g' $HOME/.config/dmscripts/config
+
+xmonad_recompile() { \
+    echo "########################"
+    echo "## Recompiling XMonad ##"
+    echo "########################"
+    xmonad --recompile
+}
+
+xmonad_recompile || error "Error recompiling Xmonad!"
+
+xmonadctl_compile() { \
+    echo "####################################"
+    echo "## Compiling the xmonadctl script ##"
+    echo "####################################"
+    ghc -dynamic "$HOME"/.xmonad/xmonadctl.hs
+}
+
+xmonadctl_compile || error "Error compiling the xmonadctl script!"
+
+PS3='Set default user shell (enter number): '
+shells=("fish" "bash" "zsh" "quit")
+select choice in "${shells[@]}"; do
+    case $choice in
+         fish | bash | zsh)
+            sudo chsh $USER -s "/bin/$choice" && \
+            echo -e "$choice has been set as your default USER shell. \
+                    \nLogging out is required for this take effect."
+            break
+            ;;
+         quit)
+            echo "User quit without changing shell."
+            break
+            ;;
+         *)
+            echo "invalid option $REPLY"
+            ;;
+    esac
+done
+cd ~
+mkdir ~/Mitongs
+cd ~/Mitongs 
+git clone https://github.com/MitasTech/dotfiles
+cd dotfiles 
+cp -r .config/conky ~/.config
+
+echo "###########################################################################"
+echo "			Put on your seatbelt. We gon fly baby!"
+echo "###########################################################################"
+sleep 2
+
+cp ~/CarnelianOS/picom.conf ~/.config/picom.conf;
+cd ~
+rm -r ~/.xmonad/xmonad.hs
+cp ~/xmonad.hs ~/.xmonad/
+
+
+
+
+echo "##############################"
+echo "## DTOS has been installed! ##"
+echo "##############################"
+done
+
+# Replace in the same state
+cd $pwd
